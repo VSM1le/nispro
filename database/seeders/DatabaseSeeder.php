c217b6a47cf4722cb0c5dc880d086df7a0b107a2
Vsm1le <earthfg@example.com>
2024-01-24 14:56:46 +0700

    seederfix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     {
        $this->call(RoleSeeder::class);
        $this->call(AdminSeeder::class);
+       $this->call(ScannerSeeder::class);
+       $this->call(PladminSeeder::class);
     }
 }
